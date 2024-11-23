d653bf3a0b2baa38171af4eac8b5b460e9702035
Bfestus <f.bigiriman@alustudent.com>
2024-11-23 16:18:09 +0200

    codes

--- wine_quality_app/lib/main.dart
+++ wine_quality_app/lib/main.dart
@@ -11,7 +11,10 @@ class WineQualityApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Wine Quality Predictor',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: 'Arial',
+      ),
       home: PredictionScreen(),
     );
   }
@@ -35,7 +38,8 @@ class _PredictionScreenState extends State<PredictionScreen> {
 
   // Function to call the FastAPI endpoint
   Future<void> _predictWineQuality() async {
-    final apiUrl = 'http://127.0.0.1:8000/predict'; // Replace with your API URL
+    final apiUrl =
+        'https://winequality-ewyy.onrender.com/predict'; // Replace with your API URL
 
     try {
       final response = await http.post(
@@ -72,58 +76,130 @@ class _PredictionScreenState extends State<PredictionScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Wine Quality Predictor'),
+        title: Text(
+          'Wine Quality Predictor',
+          style: TextStyle(
+            color: Colors.white, // Makes the title text white
+            fontWeight: FontWeight.bold, // Makes the text bold
+          ),
+        ),
+        backgroundColor: const Color.fromARGB(255, 194, 110, 143),
+        centerTitle: true,
+        elevation: 0,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              TextField(
-                controller: _fixedAcidityController,
-                decoration: InputDecoration(labelText: 'Fixed Acidity (0-15)'),
-                keyboardType: TextInputType.number,
-              ),
-              TextField(
-                controller: _volatileAcidityController,
-                decoration:
-                    InputDecoration(labelText: 'Volatile Acidity (0-2)'),
-                keyboardType: TextInputType.number,
-              ),
-              TextField(
-                controller: _citricAcidController,
-                decoration: InputDecoration(labelText: 'Citric Acid (0-1)'),
-                keyboardType: TextInputType.number,
-              ),
-              TextField(
-                controller: _alcoholController,
-                decoration: InputDecoration(labelText: 'Alcohol (0-20)'),
-                keyboardType: TextInputType.number,
-              ),
-              TextField(
-                controller: _residualSugarController,
-                decoration: InputDecoration(labelText: 'Residual Sugar (0-50)'),
-                keyboardType: TextInputType.number,
-              ),
-              TextField(
-                controller: _pHController,
-                decoration: InputDecoration(labelText: 'pH (0-14)'),
-                keyboardType: TextInputType.number,
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _predictWineQuality,
-                child: Text('Predict'),
-              ),
-              SizedBox(height: 20),
-              Text(
-                _predictionResult,
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              const Color.fromARGB(255, 183, 153, 170),
+              const Color.fromARGB(255, 231, 229, 234)
             ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Enter Wine Features',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 20),
+                ..._buildInputFields(),
+                SizedBox(height: 20),
+                Center(
+                  child: SizedBox(
+                    width: 150, // Set the desired width of the button
+                    child: ElevatedButton(
+                      onPressed: _predictWineQuality,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(
+                            255, 194, 110, 143), // Button color
+                        padding: EdgeInsets.symmetric(
+                            vertical: 12), // Adjusted height
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: Text(
+                        'Predict',
+                        style: TextStyle(
+                          fontSize: 18, // Font size
+                          color: Colors.white, // Text color
+                          fontWeight: FontWeight.bold, // Text bold
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                if (_predictionResult.isNotEmpty)
+                  Card(
+                    color: Colors.white,
+                    elevation: 4,
+                    margin: EdgeInsets.symmetric(horizontal: 16),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        _predictionResult,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.deepPurple,
+                        ),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  List<Widget> _buildInputFields() {
+    final inputFields = [
+      {"label": "Fixed Acidity (0-15)", "controller": _fixedAcidityController},
+      {
+        "label": "Volatile Acidity (0-2)",
+        "controller": _volatileAcidityController
+      },
+      {"label": "Citric Acid (0-1)", "controller": _citricAcidController},
+      {"label": "Alcohol (0-20)", "controller": _alcoholController},
+      {
+        "label": "Residual Sugar (0-50)",
+        "controller": _residualSugarController
+      },
+      {"label": "pH (0-14)", "controller": _pHController},
+    ];
+
+    return inputFields.map((field) {
+      return Card(
+        margin: EdgeInsets.only(bottom: 16),
+        elevation: 2,
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: TextField(
+            controller: field['controller'] as TextEditingController,
+            decoration: InputDecoration(
+              labelText: field['label'] as String,
+              border: OutlineInputBorder(),
+            ),
+            keyboardType: TextInputType.number,
+          ),
+        ),
+      );
+    }).toList();
   }
 }
